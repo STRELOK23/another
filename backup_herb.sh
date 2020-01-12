6df377fc89f0163deafce140b2d68ce9d3faa7e2
Piter Shell <pitpak@bk.ru>
2020-01-13 01:47:54 +0300

    bkp

--- backup_herb.sh
+++ backup_herb.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-DATE=$(date +%d%m%Y)
-WHAT=/home/c/cu76377/public_html
-WHERE=/home/c/cu76377/backups
+BNAME=backup_html_$(date +%d%m%Y)
+WHAT=$HOME/public_html
+WHERE=$HOME/backups
 
-cp -r $WHAT $WHERE/backup_html_${DATE}
+cp -r $WHAT $WHERE/$BNAME
+
+mysqldump -ucu76377_new -p`find $HOME/public_html/ -type f -name connect.php | tail -1 | xargs cat | \
+grep -Po "(?<=rd\s\=\s').*(?=')"` cu76377_new > $WHERE/$BNAME/cu76377_new.sql 2>&1 >> /dev/null
 
 for (( CNT=`ls $WHERE | wc -l`; CNT > 7; CNT-- ))
 do rm -r $WHERE/`ls -1t $WHERE | tail -1`
